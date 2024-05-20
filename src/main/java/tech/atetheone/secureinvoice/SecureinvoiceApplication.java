cdc42d1e610030d2a66d82fd221d95e4861d7590
atetheone <atevirran@gmail.com>
2024-05-20 04:38:11 +0000

    initial commit

---
+++ src/main/java/tech/atetheone/secureinvoice/SecureinvoiceApplication.java
@@ -0,0 +1,13 @@
+package tech.atetheone.secureinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureinvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureinvoiceApplication.class, args);
+	}
+
+}
