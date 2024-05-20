9cb6949c1cb3479b470dfb894dbac7b3ec086804
atetheone <atevirran@gmail.com>
2024-05-20 19:44:44 +0000

    Add user class

---
+++ src/main/java/tech/atetheone/secureinvoice/domain/User.java
@@ -0,0 +1,39 @@
+package tech.atetheone.secureinvoice.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import java.time.LocalDateTime;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@ToString
+@JsonInclude(NON_DEFAULT)
+public class User {
+  private Long userId;
+  @NotEmpty(message = "First name is required")
+  private String firstName;
+  @NotEmpty(message = "Last name is required")
+  private String lastName;
+  @NotEmpty(message = "Password is required")
+  private String password;
+  @NotEmpty(message = "Email is required")
+  @Email(message = "Invalid email")
+  private String email;
+  private String address;
+  private String phone;
+  private String title;
+  private String bio;
+  private boolean enabled;
+  private boolean nonLocked;
+  private boolean usingMfa;
+  private String imageUrl;
+  private LocalDateTime createdAt;
+}
