5ae289108873787af795e1512476591f230af15f
atetheone <atevirran@gmail.com>
2024-05-20 18:12:31 +0000

    Format sql ddl queries

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -37,9 +37,9 @@ CREATE TABLE roles (
 DROP TABLE IF EXISTS user_roles;
 
 CREATE TABLE user_roles (
-    user_role_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id     BIGINT UNSIGNED NOT NULL,
-    role_id     BIGINT UNSIGNED NOT NULL,
+    user_role_id    BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id         BIGINT UNSIGNED NOT NULL,
+    role_id         BIGINT UNSIGNED NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (role_id) REFERENCES roles (role_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -47,8 +47,8 @@ CREATE TABLE user_roles (
 DROP TABLE IF EXISTS events;
 
 CREATE TABLE events (
-    event_id    BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    type       VARCHAR(50) NOT NULL UNIQUE CHECK ( type IN (
+    event_id        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type            VARCHAR(50) NOT NULL UNIQUE CHECK ( type IN (
                                                             'LOGIN_ATTEMPT',
                                                             'LOGIN_ATTEMPT_FAILURE',
                                                             'LOGIN_ATTEMPT_SUCCESS',
@@ -59,18 +59,18 @@ CREATE TABLE events (
                                                             'PASSWORD_UPDATE',
                                                             'MFA_UPDATE'
     )),
-    description VARCHAR(255) NOT NULL
+    description     VARCHAR(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS user_events;
 
 CREATE TABLE user_events (
-    user_event_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id     BIGINT UNSIGNED NOT NULL,
-    event_id    BIGINT UNSIGNED NOT NULL,
-    device     VARCHAR(100) DEFAULT NULL,
-    ip_address  VARCHAR(50) DEFAULT NULL,
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_event_id   BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id         BIGINT UNSIGNED NOT NULL,
+    event_id        BIGINT UNSIGNED NOT NULL,
+    device          VARCHAR(100) DEFAULT NULL,
+    ip_address      VARCHAR(50) DEFAULT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
