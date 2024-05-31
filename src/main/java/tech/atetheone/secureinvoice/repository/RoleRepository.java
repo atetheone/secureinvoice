6f30c35e525a56d6d0dffd12a273d44623318aa3
atetheone <atevirran@gmail.com>
2024-05-31 00:20:11 +0000

    Add new methods to RoleRepository

--- src/main/java/tech/atetheone/secureinvoice/repository/RoleRepository.java
+++ src/main/java/tech/atetheone/secureinvoice/repository/RoleRepository.java
@@ -5,8 +5,12 @@ import tech.atetheone.secureinvoice.domain.Role;
 import java.util.Collection;
 
 public interface RoleRepository<T extends Role> {
-  void addRoleToUser(Long userId, String roleName);
   T create(T user);
   Collection<T> list(int page, int pageSize);
   T findById(Long id);
+
+  void addRoleToUser(Long userId, String roleName);
+  Role getRoleByUserId(Long userId);
+  Role getRoleByEmail(String email);
+  void updateUserRole(Long userId, String roleName);
 }
