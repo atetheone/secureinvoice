c380570ffdac672aa9dbac75744eefba002da8d4
atetheone <atevirran@gmail.com>
2024-05-31 00:19:25 +0000

    Add verification query constant

--- src/main/java/tech/atetheone/secureinvoice/query/UserQuery.java
+++ src/main/java/tech/atetheone/secureinvoice/query/UserQuery.java
@@ -3,4 +3,5 @@ package tech.atetheone.secureinvoice.query;
 public class UserQuery {
   public final static String FIND_BY_EMAIL_QUERY = "SELECT * FROM users WHERE email = :email";
   public final static String INSERT_USER_QUERY = "INSERT INTO users (first_name, last_name, password, email, address, phone, title, bio, enabled, non_locked, using_mfa, image_url, created_at) VALUES (:firstName, :lastName, :password, :email, :address, :phone, :title, :bio, :enabled, :nonLocked, :usingMfa, :imageUrl, :createdAt)";
+  public final static String INSERT_ACCOUNT_VERIFICATION_QUERY = "";
 }
