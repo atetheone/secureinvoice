5e7b098dee1747c19a44f482ed0106efafa01856
atetheone <atevirran@gmail.com>
2024-05-31 00:41:12 +0000

    Add role query class

---
+++ src/main/java/tech/atetheone/secureinvoice/query/RoleQuery.java
@@ -0,0 +1,7 @@
+package tech.atetheone.secureinvoice.query;
+
+public class RoleQuery {
+  public final static String INSERT_ROLE_TO_USER_QUERY = "";
+  public final static String SELECT_ROLE_BY_NAME_QUERY = "";
+}
+
