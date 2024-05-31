54d411a9523d1ffe883529b61d3f46dfdd3a204a
atetheone <atevirran@gmail.com>
2024-05-31 01:15:20 +0000

    Set role queries constants

--- src/main/java/tech/atetheone/secureinvoice/query/RoleQuery.java
+++ src/main/java/tech/atetheone/secureinvoice/query/RoleQuery.java
@@ -1,7 +1,7 @@
 package tech.atetheone.secureinvoice.query;
 
 public class RoleQuery {
-  public final static String INSERT_ROLE_TO_USER_QUERY = "";
-  public final static String SELECT_ROLE_BY_NAME_QUERY = "";
+  public final static String INSERT_ROLE_TO_USER_QUERY = "INSERT INTO user_roles (user_id, role_id) VALUES (:user_id, :role_id)";
+  public final static String SELECT_ROLE_BY_NAME_QUERY = "SELECT * FROM roles WHERE role_name = :role_name";
 }
 
